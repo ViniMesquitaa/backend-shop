4e4dc11efa8495fa3770d974eef4414be0283a94
Vinicius <vinniciussramallho98@gmail.com>
2025-04-25 17:10:28 -0300

    add methods to delete and getAllProductsInCart

--- src/main/java/com/shop/cleaning/dev/services/ClientService.java
+++ src/main/java/com/shop/cleaning/dev/services/ClientService.java
@@ -2,14 +2,19 @@ package com.shop.cleaning.dev.services;
 
 import com.shop.cleaning.dev.dtos.DtoClientRequest;
 import com.shop.cleaning.dev.dtos.DtoClientResponse;
+import com.shop.cleaning.dev.dtos.DtoItemCartResponse;
+import com.shop.cleaning.dev.dtos.DtoProductResponse;
 import com.shop.cleaning.dev.entities.Address;
 import com.shop.cleaning.dev.entities.Client;
 import com.shop.cleaning.dev.repositories.ClientRepository;
+import com.shop.cleaning.dev.repositories.ItemCartRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
@@ -20,10 +25,12 @@ import java.util.stream.Collectors;
 public class ClientService {
 
     private final ClientRepository clientRepository;
+    private final ItemCartRepository itemCartRepository;
 
     @Autowired
-    public ClientService(ClientRepository clientRepository) {
+    public ClientService(ClientRepository clientRepository, ItemCartRepository itemCartRepository) {
         this.clientRepository = clientRepository;
+        this.itemCartRepository = itemCartRepository;
     }
 
 
@@ -38,14 +45,45 @@ public class ClientService {
     public DtoClientResponse getClientInfoById(UUID clientId) {
         Client client = clientRepository.findById(clientId)
                 .orElseThrow(() -> new RuntimeException("Client not found"));
+
         return new DtoClientResponse(client.getId(), client.getName(), client.getAddress(), client.getCreationTimesStamp(), client.getUpdateTimesStamp());
     }
 
     //Buscar todos os clientes "cadastrados"
     public List<DtoClientResponse> getAllClients() {
         return clientRepository.findAll().stream()
-                .map(client -> new DtoClientResponse(client.getId(), client.getName(), client.getAddress(),client.getCreationTimesStamp(), client.getUpdateTimesStamp() ))
+                .map(client -> new DtoClientResponse(client.getId(), client.getName(), client.getAddress(), client.getCreationTimesStamp(), client.getUpdateTimesStamp()))
                 .collect(Collectors.toList());
+    }
+
+    public List<DtoItemCartResponse> getProductsCartByClient(UUID clientId) {
+        return itemCartRepository.findByClientId(clientId).stream()
+                .map(itemCart -> new DtoItemCartResponse(
+                        itemCart.getId(),
+                        new DtoProductResponse(
+                                itemCart.getProduct().getId(),
+                                itemCart.getProduct().getImg(),
+                                itemCart.getProduct().getName(),
+                                itemCart.getProduct().getDescription(),
+                                itemCart.getProduct().getPrice(),
+                                itemCart.getProduct().getAtivo()
+                        ),
+                        itemCart.getItemQuantity(),
+                        itemCart.getPrecoUnitario(),
+                        itemCart.getTotalItem(),
+                        itemCart.getAtivo()
+                ))
+                .collect(Collectors.toList());
+    }
+
+
+
+    //deletar cliente pelo id
+    public void deleteClientById(UUID clientId) {
+        if (!clientRepository.existsById(clientId)) {
+            throw new EntityNotFoundException("Client not found or no exists");
+        }
+        clientRepository.deleteById(clientId);
     }
 
 
