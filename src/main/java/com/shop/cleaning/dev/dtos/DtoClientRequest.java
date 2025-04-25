b3a3c5753a2a0efc94ae44588e25f8085cae39eb
Vinicius <vinniciussramallho98@gmail.com>
2025-04-25 14:38:11 -0300

    dtos to requests client

--- src/main/java/com/shop/cleaning/dev/dtos/DtoClientRequest.java
+++ src/main/java/com/shop/cleaning/dev/dtos/DtoClientRequest.java
@@ -1,4 +1,23 @@
 package com.shop.cleaning.dev.dtos;
 
-public record DtoClientRequest() {
+import com.shop.cleaning.dev.entities.Address;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public record DtoClientRequest(
+                                @Id
+                                UUID id,
+                                @NotBlank
+                                String name,
+                                Address address
+
+
+) {
 }
