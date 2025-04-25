6821d0c9b49a5391b5a2f0e2a6c19156145ba25a
Vinicius <vinniciussramallho98@gmail.com>
2025-04-25 17:11:29 -0300

    new reference and new getters, lombok is not cool :/

--- src/main/java/com/shop/cleaning/dev/entities/ItemCart.java
+++ src/main/java/com/shop/cleaning/dev/entities/ItemCart.java
@@ -11,7 +11,6 @@ import java.util.UUID;
 
 @Getter
 @Setter
-@NoArgsConstructor
 
 @Entity
 @Table(name = "itens_carrinho")
@@ -24,6 +23,10 @@ public class ItemCart {
     @ManyToOne
     @JoinColumn(name = "product_id")
     private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
 
     private Integer itemQuantity;
 
@@ -38,4 +41,66 @@ public class ItemCart {
 
     private boolean ativo;
 
+    public boolean getAtivo() {
+        return ativo;
+    }
+
+    public ItemCart() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public BigDecimal getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(BigDecimal precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public Cart getCartShop() {
+        return cartShop;
+    }
+
+    public void setCartShop(Cart cartShop) {
+        this.cartShop = cartShop;
+    }
+
+    public Integer getItemQuantity() {
+        return itemQuantity;
+    }
+
+    public void setItemQuantity(Integer itemQuantity) {
+        this.itemQuantity = itemQuantity;
+    }
+
+    public BigDecimal getTotalItem() {
+        return totalItem;
+    }
+
+    public void setTotalItem(BigDecimal totalItem) {
+        this.totalItem = totalItem;
+    }
 }
--- src/main/java/com/shop/cleaning/dev/entities/Product.java
+++ src/main/java/com/shop/cleaning/dev/entities/Product.java
@@ -29,4 +29,61 @@ public class Product {
 
     private Integer quantityStock;
 
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public Integer getQuantityStock() {
+        return quantityStock;
+    }
+
+    public void setQuantityStock(Integer quantityStock) {
+        this.quantityStock = quantityStock;
+    }
+
+
 }
