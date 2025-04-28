d1437f5a9db625d266e29753d9972e06c547ed1c
Vinicius <vinniciussramallho98@gmail.com>
2025-04-28 15:43:59 -0300

    builing: create new functions
    edit client info, annotation transctional

--- src/main/java/com/shop/cleaning/dev/services/ClientService.java
+++ src/main/java/com/shop/cleaning/dev/services/ClientService.java
@@ -1,20 +1,15 @@
 package com.shop.cleaning.dev.services;
 
-import com.shop.cleaning.dev.dtos.DtoClientRequest;
-import com.shop.cleaning.dev.dtos.DtoClientResponse;
-import com.shop.cleaning.dev.dtos.DtoItemCartResponse;
-import com.shop.cleaning.dev.dtos.DtoProductResponse;
-import com.shop.cleaning.dev.entities.Address;
+import com.shop.cleaning.dev.dtos.requestDtos.DtoClientRequest;
+import com.shop.cleaning.dev.dtos.responseDtos.ClientUpdateDtoResponse;
+import com.shop.cleaning.dev.dtos.responseDtos.DtoClientResponse;
 import com.shop.cleaning.dev.entities.Client;
 import com.shop.cleaning.dev.repositories.ClientRepository;
-import com.shop.cleaning.dev.repositories.ItemCartRepository;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
@@ -25,16 +20,16 @@ import java.util.stream.Collectors;
 public class ClientService {
 
     private final ClientRepository clientRepository;
-    private final ItemCartRepository itemCartRepository;
 
     @Autowired
-    public ClientService(ClientRepository clientRepository, ItemCartRepository itemCartRepository) {
+    public ClientService(ClientRepository clientRepository) {
         this.clientRepository = clientRepository;
-        this.itemCartRepository = itemCartRepository;
+
     }
 
 
     //criar o client
+    @Transactional
     public UUID createClient(DtoClientRequest request) {
         Client client = new Client(UUID.randomUUID(), request.name(), request.address(), Instant.now(), null);
         var userSaved = clientRepository.save(client);
@@ -55,30 +50,18 @@ public class ClientService {
                 .map(client -> new DtoClientResponse(client.getId(), client.getName(), client.getAddress(), client.getCreationTimesStamp(), client.getUpdateTimesStamp()))
                 .collect(Collectors.toList());
     }
+    public ClientUpdateDtoResponse updateClients(UUID uuid, DtoClientRequest request) {
+        Client client = clientRepository.findById(uuid).orElseThrow(() -> new RuntimeException("Client not found"));
 
-    public List<DtoItemCartResponse> getProductsCartByClient(UUID clientId) {
-        return itemCartRepository.findByClientId(clientId).stream()
-                .map(itemCart -> new DtoItemCartResponse(
-                        itemCart.getId(),
-                        new DtoProductResponse(
-                                itemCart.getProduct().getId(),
-                                itemCart.getProduct().getImg(),
-                                itemCart.getProduct().getName(),
-                                itemCart.getProduct().getDescription(),
-                                itemCart.getProduct().getPrice(),
-                                itemCart.getProduct().getAtivo()
-                        ),
-                        itemCart.getItemQuantity(),
-                        itemCart.getPrecoUnitario(),
-                        itemCart.getTotalItem(),
-                        itemCart.getAtivo()
-                ))
-                .collect(Collectors.toList());
+        client.setName(request.name());
+        client.setAddress(request.address());
+        Client updatedClient = clientRepository.save(client);
+
+        return new ClientUpdateDtoResponse(updatedClient.getId(), updatedClient.getName(), updatedClient.getAddress());
     }
 
-
-
     //deletar cliente pelo id
+    @Transactional
     public void deleteClientById(UUID clientId) {
         if (!clientRepository.existsById(clientId)) {
             throw new EntityNotFoundException("Client not found or no exists");
