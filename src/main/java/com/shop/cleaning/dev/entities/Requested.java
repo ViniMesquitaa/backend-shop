3f0145d29b9fdb7de6b8b874d827a3ca4d4749a7
Vinicius <vinniciussramallho98@gmail.com>
2025-04-28 15:42:08 -0300

    new class requested

--- src/main/java/com/shop/cleaning/dev/entities/Requested.java
+++ src/main/java/com/shop/cleaning/dev/entities/Requested.java
@@ -1,4 +1,29 @@
 package com.shop.cleaning.dev.entities;
 
+import com.shop.cleaning.dev.entities.enuns.StatusRequestedEnum;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+@Table(name = "pedido")
+@Entity
 public class Requested {
+    @Id
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    @CreationTimestamp
+    private Instant createTimeStamp;
+    private BigDecimal totalPrice;
+    private StatusRequestedEnum status;
+
+    @OneToMany(mappedBy = "requested", cascade = CascadeType.ALL)
+    public List<ItemCart> items = new ArrayList<>();
 }
